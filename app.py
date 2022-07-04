88337b9d5336e0dfe17122a521b54d47a7a3db18
AasthaSharma-git <67312804+AasthaSharma-git@users.noreply.github.com>
2022-07-04 11:30:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,jsonify,request
+from Class125 import get_prediction
+
+app=Flask(__name__)
+
+@app.route("/predict-digit",methods=["POST"])
+
+def predict_digit():
+    image = request.files.get("digit")
+    prediction =get_prediction(image)
+    return jsonify({
+        "prediction": prediction
+    })
+
+if __name__ == "__main__":
+   app.run(debug=True)
+
